e85a74ec0c7a139dd184ca047061239ea9ea9b48
zouyaoji <370681295@qq.com>
2025-03-29 18:33:26 +0800

    chore: :hammer: 更新publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,7 @@
 ###
  # @Author: zouyaoji@https://github.com/zouyaoji
  # @Date: 2021-09-16 09:28:13
- # @LastEditTime: 2025-03-29 16:43:18
+ # @LastEditTime: 2025-03-29 18:32:29
  # @LastEditors: zouyaoji 370681295@qq.com
  # @Description:
  # @FilePath: \vue-maplibre\scripts\publish.sh
@@ -12,12 +12,12 @@
 set -e
 
 pnpm i --frozen-lockfile
-# pnpm update:version
+pnpm update:version
 
 pnpm build
 
 cd dist/vue-maplibre
-# npm publish --tag latest --access public --registry ${REGISTRY}
+npm publish --tag latest --access public --registry ${REGISTRY}
 cd -
 
 echo "✅ Publish completed"
