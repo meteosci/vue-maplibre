dc4c0ca72655253039047c4266e087c37d0eaef2
zouyaoji <370681295@qq.com>
2025-03-29 10:23:00 +0000

    website deploy

---
+++ assets/index.md.a9c8d0a4.js
@@ -0,0 +1,1 @@
+import{o as e,c as t}from"./app.38f957ed.js";const _='{"title":"A Vue 3 Map Framework","description":"","frontmatter":{"title":"A Vue 3 Map Framework","lang":"en-US","page":true},"headers":[],"relativePath":"index.md","lastUpdated":1743243451000}',a={},c=Object.assign(a,{__name:"index",setup(r){return(n,o)=>(e(),t("div"))}});export{_ as __pageData,c as default};
