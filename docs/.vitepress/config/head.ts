4e8eef2a403e2c6ea3e368a9d51070ecbe8e9493
zouyaoji <370681295@qq.com>
2025-11-21 21:55:46 +0800

    chore: :hammer: 更新meta

--- docs/.vitepress/config/head.ts
+++ docs/.vitepress/config/head.ts
@@ -5,7 +5,7 @@ export const head: HeadConfig[] = [
     'link',
     {
       rel: 'icon',
-      href: '/images/element-plus-logo-small.svg',
+      href: '/images/vue-maplibre-logo-mini.svg',
       type: 'image/svg+xm'
     }
   ],
@@ -13,7 +13,7 @@ export const head: HeadConfig[] = [
     'meta',
     {
       property: 'og:image',
-      content: '/images/element-plus-og-image.png'
+      content: '/images/vue-maplibre-logo.svg'
     }
   ],
   [
@@ -34,21 +34,37 @@ export const head: HeadConfig[] = [
     'meta',
     {
       property: 'og:description',
-      content: 'A Vue 3 based component library for designers and developers'
+      content: 'A Vue3 based component library of MaplibreGL for developers.'
     }
   ],
   [
     'meta',
     {
+      name: 'algolia-site-verification',
+      content: '872D530159005E01'
+    }
+  ],
+  // 百度资源
+  [
+    'meta',
+    {
       name: 'baidu-site-verification',
-      content: 'codeva-q5gBxYcfOs'
+      content: 'codeva-ZmBRda3dIC'
+    }
+  ],
+  // 百度统计
+  [
+    'script',
+    {
+      async: 'true',
+      src: 'https://hm.baidu.com/hm.js?80cf722845bf724c5b85845b182669b3'
     }
   ],
   [
     'script',
     {
       async: 'true',
-      src: 'https://www.googletagmanager.com/gtag/js?id=UA-175337989-1'
+      src: 'https://www.googletagmanager.com/gtag/js?id=UA-113302031-1'
     }
   ],
   [
@@ -68,13 +84,13 @@ export const head: HeadConfig[] = [
     `window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'UA-175337989-1');`
+gtag('config', 'G-NV0Y3PCY1J');`
   ],
   [
     'script',
     {
       async: 'true',
-      src: 'https://www.googletagmanager.com/gtag/js?id=G-M74ZHEQ1M1'
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-0DRFCYSG8M'
     }
   ],
   [
@@ -85,7 +101,7 @@ gtag('config', 'UA-175337989-1');`
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
 
-      gtag('config', 'G-M74ZHEQ1M1');
+      gtag('config', 'G-0DRFCYSG8M');
     `
   ],
   [
